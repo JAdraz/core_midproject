986df7f28cafba71d4d2d687fa32a392e3c6ea5a
Jesus <jesus.adraz@eazychart.dev>
2023-02-23 11:25:57 -0400

    routers

--- api/router/endpoints.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/{country}")
-def pais(country):
-    return {
-        "Pais" : country
-    }
--- api/router/germany_covid.py
+++ api/router/germany_covid.py
@@ -2,3 +2,6 @@ from fastapi import APIRouter
 from mongo.connection import db
 
 router = APIRouter()
+
+# @router.get("/cases/{country}/{MM/DD/AA}")
+# def country_cases(country):
