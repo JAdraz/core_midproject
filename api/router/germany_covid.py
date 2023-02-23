249840e7478dffb865d0f6fffd21c6229c68cbca
Jesus <jesus.adraz@eazychart.dev>
2023-02-23 13:51:45 -0400

    creating routers for cases, deaths and recovered

--- api/router/germany_covid.py
+++ api/router/germany_covid.py
@@ -1,7 +1,21 @@
 from fastapi import APIRouter
 from mongo.connection import db
+from bson import json_util
+from json import loads
 
 router = APIRouter()
 
-# @router.get("/cases/{country}/{MM/DD/AA}")
-# def country_cases(country):
+@router.get("/all/germany/cases/")
+def confirmed_cases():
+    res = db["germany_covid_confirmed"].find({})[:10]
+    return loads(json_util.dumps(res))
+
+@router.get("/all/germany/deaths/")
+def confirmed_cases():
+    res = db["germany_covid_deaths"].find({})
+    return loads(json_util.dumps(res))
+
+@router.get("/all/germany/recovered/")
+def confirmed_cases():
+    res = db["germany_covid_recovered"].find({})
+    return loads(json_util.dumps(res))
