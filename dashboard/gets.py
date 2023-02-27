3ac76da829393872ff98b2cbd0b2bb1901f08708
Jesus <jesus.adraz@eazychart.dev>
2023-02-27 18:55:05 -0400

    creating gets functions

--- dashboard/gets.py
+++ dashboard/gets.py
@@ -2,11 +2,11 @@ from url import url
 import requests
 
 
-def get_sex():
+def get_cases():
     return requests.get(url+"/all/germany/cases/").json()
 
-def get_island():
+def get_deaths():
     return requests.get(url+"/all/germany/deaths/").json()
 
-def get_species():
+def get_recovered():
     return requests.get(url+"/all/germany/recovered/").json()
