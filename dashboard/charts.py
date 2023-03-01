97649ef84dc52aeb0c5ec0576fd971a8431bc48e
Jesus <jesus.adraz@eazychart.dev>
2023-03-01 15:46:14 -0400

    act

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -1,15 +1,15 @@
 import streamlit as st
+import pandas as pd
 from datetime import datetime
-import matplotlib.pyplot as plt
 
 # This function creates a linear chart for confirmed, deaths and recovered cases
 def chart1(data):
-    raw1 = list(data[0].keys())
-    raw2 = list(data[0].values())
+    # raw1 = list(data[0].keys())
+    # raw2 = list(data[0].values())
 
-    dates = [datetime.date(datetime.strptime(date_string, '%m/%d/%y')) for date_string in raw1[3:]]
-    cases = [int(n) for n in raw2[3:]]
-
-    return st.plotly_chart(cases, dates)
-    # plt.plot(dates, cases), plt.xlabel('Dates'), plt.ylabel('Confirmed Cases'), plt.yticks([1_000_000, 2_000_000, 3_000_000, 4_000_000], ["1M", "2M", "3M", "4M"]), plt.grid("minor")
-    # return plt.show()
+    # dates = [datetime.date(datetime.strptime(date_string, '%m/%d/%y')) for date_string in raw1[3:]]
+    # cases = [int(n) for n in raw2[3:]]
+    for i in ["Country/Region", "Lat", "Long"]:
+        data.pop(i)
+    pd.DataFrame.from_records([data])
+    return st.bar_chart(data)
--- dashboard/main.py
+++ dashboard/main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from gets import get_cases, get_deaths, get_recovered
 from charts import chart1
+import matplotlib.pyplot as plt
 
 # Header
 st.title("COVID-19 Dashboard")
 
 data = get_cases()
-chart1(data)
+st.text(data[0])
+chart1(data[0])
