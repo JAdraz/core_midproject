15424e169a89ce0076754bff04054e398188e967
Jesus <jesus.adraz@eazychart.dev>
2023-03-02 17:43:44 -0400

    adding capitalize to gets functions

--- dashboard/gets.py
+++ dashboard/gets.py
@@ -2,11 +2,14 @@ from url import url
 import requests
 
 
-def get_cases():
-    return requests.get(url+"/all/germany/cases/").json()
+def get_confirmed(a:str):
+    country = a.capitalize()
+    print(requests.get(url+f"/confirmed/{country}"))
 
-def get_deaths():
-    return requests.get(url+"/all/germany/deaths/").json()
+def get_deaths(a:str):
+    country = a.capitalize()
+    return requests.get(url+f"/deaths/{country}").json()
 
-def get_recovered():
-    return requests.get(url+"/all/germany/recovered/").json()
+def get_recovered(a:str):
+    country = a.capitalize()
+    return requests.get(url+f"/recovered/{country}").json()
