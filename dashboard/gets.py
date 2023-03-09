d96a97534ca58b69c7b88bd9243bd3c21be19c50
Jesus <jesus.adraz@eazychart.dev>
2023-03-08 21:10:43 -0400

    comment unused gets

--- dashboard/gets.py
+++ dashboard/gets.py
@@ -2,17 +2,17 @@ from url import url
 import requests
 
 
-def get_confirmed(a:str):
-    country = a.capitalize()
-    return (requests.get(url+f"/confirmed/{country}")).json()
+# def get_confirmed(a:str):
+#     country = a.capitalize()
+#     return (requests.get(url+f"/confirmed/{country}")).json()
 
-def get_deaths(a:str):
-    country = a.capitalize()
-    return requests.get(url+f"/deaths/{country}").json()
+# def get_deaths(a:str):
+#     country = a.capitalize()
+#     return requests.get(url+f"/deaths/{country}").json()
 
-def get_recovered(a:str):
-    country = a.capitalize()
-    return requests.get(url+f"/recovered/{country}").json()
+# def get_recovered(a:str):
+#     country = a.capitalize()
+#     return requests.get(url+f"/recovered/{country}").json()
 
 def get_countries():
     return (requests.get(url+f"/countries")).json()
